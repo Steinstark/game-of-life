3815fbe56b486c1a2b4dcd9f571ca9dcf48b66d9
Henrik Josefsson <henrik@hejorden.se>
2020-07-08 14:17:40 +0200

    Add test cases
    Added test cases to verify the correct implementation of game of life rules.

--- src/test/java/game/GameOfLifeStateTest.java
+++ src/test/java/game/GameOfLifeStateTest.java
@@ -9,7 +9,105 @@ import java.util.Set;
 import org.junit.jupiter.api.Test;
 
 class GameOfLifeStateTest {
+	
+	/**
+	 * Verifies that a colony with no neighbors is dead after
+	 * {@link GameOfLifeState#update()}.
+	 * 
+	 * #
+	 */
+	@Test
+	public void zeroNeighborTest() {
+		List<Position> colonies = new ArrayList<>(1);
+		colonies.add(new Position(5, 5));
+		GameOfLifeState state = new GameOfLifeState(11, 11, colonies);
+		state.update();
+		assertTrue(state.getColonies().isEmpty());
+	}
+	
+	/**
+	 * Verifies that a colony with exactly one neighbor is dead after
+	 * {@link GameOfLifeState#update()}.
+	 * 
+	 * #
+	 * #
+	 */
+	@Test
+	public void oneNeighborTest() {
+		List<Position> colonies = new ArrayList<>(2);
+		colonies.add(new Position(5, 5));
+		colonies.add(new Position(5, 6));
+		GameOfLifeState state = new GameOfLifeState(11, 11, colonies);
+		state.update();
+		assertTrue(state.getColonies().isEmpty());
+	}
+	
 
+	/**
+	 * Verifies that a colony with exactly two neighbors is alive after
+	 * {@link GameOfLifeState#update()}.
+	 * 
+	 *  #
+	 * # #
+	 *  #
+	 */
+	@Test
+	public void twoNeighborTest() {
+		List<Position> colonies = new ArrayList<>(4);
+		colonies.add(new Position(5, 5));
+		colonies.add(new Position(4, 6));
+		colonies.add(new Position(6, 6));
+		colonies.add(new Position(5, 7));
+		GameOfLifeState state = new GameOfLifeState(11, 11, colonies);
+		assertFalse(state.update());
+		Set<Position> actualColonies = state.getColonies();
+		assertTrue(actualColonies.size() == colonies.size());
+		assertTrue(colonies.containsAll(actualColonies));
+		
+	}
+	
+	/**
+	 * Verifies that a new colony is born in a square with exactly three neighboring
+	 * colonies after {@link GameOfLifeState#update()}.
+	 * 
+	 * # #
+	 *  #
+	 */
+	@Test
+	public void threeNeighborTest() {
+		List<Position> colonies = new ArrayList<>(3);
+		colonies.add(new Position(4, 5));
+		colonies.add(new Position(6, 5));
+		colonies.add(new Position(5, 6));
+		GameOfLifeState state = new GameOfLifeState(11, 11, colonies);
+		state.update();
+		Set<Position> actualColonies = state.getColonies();
+		assertTrue(actualColonies.size() == 2);
+		assertTrue(actualColonies.contains(new Position(5, 5)));
+	}
+	
+	/**
+	 * Verifies that a colony with neighbors > 3 is dead after
+	 * {@link GameOfLifeState#update()}.
+	 * 
+	 *  #
+	 * ###
+	 *  # 
+	 */
+	@Test
+	public void tooManyNeighborsTest() {
+		List<Position> colonies = new ArrayList<>(5);
+		colonies.add(new Position(5, 5));
+		colonies.add(new Position(4, 6));
+		colonies.add(new Position(5, 6));
+		colonies.add(new Position(6, 6));
+		colonies.add(new Position(5, 7));
+		GameOfLifeState state = new GameOfLifeState(11, 11, colonies);
+		state.update();
+		Set<Position> actualColonies = state.getColonies();
+		assertFalse(actualColonies.contains(new Position(5,6)));		
+	}
+	
 	/**
 	 * Verifies the expected behavior of {@link GameOfLifeState#update()} for a
 	 * simple square shape.
@@ -29,7 +127,7 @@ class GameOfLifeStateTest {
 			assertFalse(state.update());
 		}
 		Set<Position> actualColonies = state.getColonies();
-		assertTrue(actualColonies.size() == 4);
+		assertTrue(actualColonies.size() == colonies.size());
 		assertTrue(colonies.containsAll(actualColonies));
 	}
 
